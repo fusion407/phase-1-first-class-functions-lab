1ecef246f963ff29f5c389859f1fad778123a4be
fusion407 <fusion407@gmail.com>
2022-06-29 21:56:19 -0500

    commit tree 7c62b82309a845b430ac87d7a6b708519c00e9a8

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0,2);
+}
+function returnLastTwoDrivers(drivers) {
+    return drivers.slice(2,4);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(price) {
+    let farePrice = function() {
+        return price * price;
+    }
+    return farePrice;
+}
+function fareDoubler(dub = createFareMultiplier()) {
+    return dub * 2;
+}
+function fareTripler(trip = createFareMultiplier()) {
+    return trip * 3;
+}
+function selectDifferentDrivers(drivers = selectingDrivers, fn) {
+    return fn(drivers);
+}
